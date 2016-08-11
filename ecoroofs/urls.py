2cfab02988f133ace7723c354e751a1af0798d8f
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-11 16:18:47 -0700

    Use ARCUtils' CASAdminSite instead of Django's default

--- ecoroofs/urls.py
+++ ecoroofs/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib import admin
 
+from arcutils import admin
 import arcutils.cas.urls
 
 from . import views
@@ -9,11 +9,11 @@ from . import views
 
 urlpatterns = [
     # Home
-    url(r'^$', views.JSAppView.as_view(template_name=settings.HOME_PAGE_TEMPLATE)),
+    url(r'^$', views.JSAppView.as_view(template_name=settings.HOME_PAGE_TEMPLATE), name='home'),
     url(r'^admin$', views.JSAppView.as_view(template_name='admin.html')),
 
     # Admin
-    url(r'^django-admin/', admin.site.urls),
+    url(r'^django-admin/', admin.cas_site.urls),
 
     # Auth
     url(r'', include(arcutils.cas.urls)),
