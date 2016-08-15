d892c58e7756bee793aafcef02d621d4628940a5
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-15 12:22:51 -0700

    Plug in a DRF router
    The plan is to use DRF `ViewSet`s for most views.

--- ecoroofs/urls.py
+++ ecoroofs/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls import include, url
 
 from arcutils import admin
+from arcutils.drf.routers import DefaultRouter
 import arcutils.cas.urls
 
 from . import views
+from .locations.views import LocationViewSet
 
 
 urlpatterns = [
@@ -18,3 +20,6 @@ urlpatterns = [
     # Auth
     url(r'', include(arcutils.cas.urls)),
 ]
+
+router = DefaultRouter()
+urlpatterns += router.urls
