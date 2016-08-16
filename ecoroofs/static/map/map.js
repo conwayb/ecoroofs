9bfd008bddda008767e8f2d1bd2f6d2e1a6385f8
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-15 17:56:46 -0700

    Add locations layer to map
    ECO-19 #in-progress #time 1h

--- ecoroofs/static/map/map.js
+++ ecoroofs/static/map/map.js
@@ -1,8 +1,13 @@
 import ol from 'ol';
 
+const projectionCode = 'EPSG:3857';
+
 export default class Map extends ol.Map {
     constructor (options) {
         const center = ol.proj.fromLonLat(options.view.center);
+        const serverOptions = options.server;
+        const mapServerBaseURL = serverOptions.baseURL;
+        const workspace = `ecoroofs-${options.env}`;
 
         const baseLayers = [
             new ol.layer.Tile({
@@ -10,7 +15,11 @@ export default class Map extends ol.Map {
             })
         ];
 
+        const featureLayers = [
+            makeFeatureLayer(mapServerBaseURL, workspace, 'locations', 'Locations')
+        ];
 
+        const allLayers = [].concat(baseLayers, featureLayers);
 
         const view = new ol.View({
             center: center,
@@ -20,9 +29,34 @@ export default class Map extends ol.Map {
         });
 
         super({
-            layers: baseLayers,
+            layers: allLayers,
             target: options.target,
             view: view
         });
     }
+}
+
+function makeFeatureLayer (baseURL, workspace, layerName, label) {
+    const url = `${baseURL}/wfs/${workspace}`;
+    const typeName = `${workspace}:${layerName}`;
+    const baseParams = [
+        'service=WFS',
+        'version=1.1.0',
+        'request=GetFeature',
+        `typeName=${typeName}`,
+        `srsName=${projectionCode}`,
+        'outputFormat=application/json'
+    ].join('&');
+    return new ol.layer.Vector({
+        label: label,
+        source: new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            strategy: ol.loadingstrategy.bbox,
+            url: function (extent) {
+                const bboxParam = `${extent.join(',')},${projectionCode}`;
+                const requestParams = `${baseParams}&bbox=${bboxParam}`;
+                return `${url}?${requestParams}`;
+            }
+        })
+    })
 }
