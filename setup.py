fcd1500c10a7042258177e0fa4175d9fb63a06b1
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-23 16:54:39 -0700

    Sort dependencies in install_requires

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ setup(
     install_requires=[
         'django>=1.10.1',
         'django-arcutils>=2.11.1',
+        'django-haystack>=2.4.1,<2.5',
         'django-local-settings>=1.0b1',
         'django-pgcli>=0.0.2',
         'djangorestframework>=3.4.1',
+        'elasticsearch>=1.9.0,<2.0',
         'psycopg2>=2.6.2',
         'pytz>=2016.6.1',
-        'django-haystack>=2.4.1,<2.5',
-        'elasticsearch>=1.9.0,<2.0',
         'psu.oit.arc.tasks',
     ],
     extras_require={
