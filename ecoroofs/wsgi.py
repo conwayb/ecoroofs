0a16bda1554fc7ce59de736adbcf0b359ef2ce85
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-24 14:29:16 -0700

    Improve wsgi application module
    Make it more flexible by allowing the root and virtualenv directories
    to be specified via environment variables (WSGI_ROOT & WSGI_VENV). This
    supports running the project in other environments, such as Docker.
    
    Also, derive the default DJANGO_SETTINGS_MODULE value from the location
    of the wsgi module instead of forcing it to be set explicitly.
    
    TODO: Copy changes to ARCUtils' default wsgi.py.

--- ecoroofs/wsgi.py
+++ ecoroofs/wsgi.py
@@ -3,26 +3,35 @@ import site
 import sys
 
 
-def create_wsgi_application(root, settings_module=None, local_settings_file=None):
-    """Create a WSGI application anchored at ``root``.
+def create_wsgi_application(default_django_settings_module=None):
+    """Create a WSGI application.
 
-    ``root`` must contain a virtualenv at ``./.env``.
-
-    ``settings_module`` is only used if the ``DJANGO_SETTINGS_MODULE``
-    environment variable is not already set.
-
-    Likewise, ``local_settings_file`` is only used if the environment
-    variable ``LOCAL_SETTINGS_FILE`` is not already set. The default
-    value for this is ``{root}/local.cfg``. If a project doesn't use
-    ``django-local-settings``, this will have no effect.
+    Args:
+        default_django_settings_module: This will be used if the
+            ``DJANGO_SETTINGS_MODULE`` environment variable isn't set.
+            If neither of these is set, the settings module will be
+            set to ``'{directory containing wsgi.py}.settings'``.
 
     """
-    if local_settings_file is None:
-        local_settings_file = os.path.join(root, 'local.cfg')
+    # Directory containing this wsgi.py file.
+    containing_dir = os.path.dirname(__file__)
+
+    # Just the base name (with no path) of the containing directory.
+    dir_name = os.path.basename(containing_dir)
+
+    # The top level project directory, assuming that this wsgi.py file is in
+    # the project's top level package directory.
+    default_root = os.path.dirname(containing_dir)
+
+    if default_django_settings_module is None:
+        default_django_settings_module = '{dir_name}.settings'.format_map(locals())
+
+    root = os.environ.get('WSGI_ROOT', default_root)
+    venv = os.environ.get('WSGI_VENV', os.path.join(root, '.env'))
 
     major, minor = sys.version_info[:2]
-    site_packages = 'lib/python{major}.{minor}/site-packages'.format(**locals())
-    site_packages = os.path.join(root, '.env', site_packages)
+    site_packages_rel_path = 'lib/python{major}.{minor}/site-packages'.format(**locals())
+    site_packages = os.path.join(venv, site_packages_rel_path)
 
     if not os.path.isdir(site_packages):
         raise NotADirectoryError(
@@ -36,8 +45,8 @@ def create_wsgi_application(root, settings_module=None, local_settings_file=None
     new_sys_path = [item for item in sys.path if item not in old_sys_path]
     sys.path = new_sys_path + old_sys_path
 
-    os.environ.setdefault('LOCAL_SETTINGS_FILE', local_settings_file)
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    os.environ.setdefault('LOCAL_SETTINGS_FILE', os.path.join(root, 'local.cfg'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', default_django_settings_module)
 
     from django.conf import settings
     from django.core.management import call_command
@@ -54,6 +63,4 @@ def create_wsgi_application(root, settings_module=None, local_settings_file=None
     return app
 
 
-root = os.path.dirname(os.path.dirname(__file__))
-settings_module = 'ecoroofs.settings'
-application = create_wsgi_application(root, settings_module)
+application = create_wsgi_application('ecoroofs.settings')
