3bf1c49ef32f7abd8ea8e2b9d9c6f20603f3d153
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-09-15 16:58:48 -0700

    Show total square footage in toolbar
    Not sure this is the best place for it. Could probably also use some
    styling.
    
    ECO-16 #in-progress #time 15m

--- ecoroofs/static/toolbar/toolbar.component.js
+++ ecoroofs/static/toolbar/toolbar.component.js
@@ -2,16 +2,20 @@ import template from './toolbar.html!text';
 
 const ToolbarComponent = {
     template: template,
-    controller: function ($mdSidenav, appConfig) {
+    controller: function ($http, $mdSidenav, appConfig) {
         const sidenavId = 'sidenav-left';
         this.appConfig = appConfig;
         this.user = appConfig.user;
+        this.totalSquareFootage = null;
         this.openSidenav = () => {
             $mdSidenav(sidenavId).open();
         };
         this.closeSidenav = () => {
             $mdSidenav(sidenavId).close();
         };
+        $http.get('/_/locations/square-footage').then((response) => {
+            this.totalSquareFootage = response.data.total;
+        });
     }
 };
 
