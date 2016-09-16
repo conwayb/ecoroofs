b47094e836c734798e76dca2bc48ec72c5045e77
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-09-16 16:31:27 -0700

    Flesh out Location serializer a bit
    Use nested/related serializer fields for related model fields. This
    means the data for building use, neighborhood, and watershed will be
    included, not just their PKs.

--- ecoroofs/locations/serializers.py
+++ ecoroofs/locations/serializers.py
@@ -1,8 +1,35 @@
+from rest_framework import serializers
+
 from ..serializers import ModelSerializer
-from .models import Location
+from ..neighborhoods.models import Neighborhood
+
+from .models import *  # noqa
+
+
+class BuildingUseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BuildingUse
+
+
+class NeighborhoodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Neighborhood
+        fields = ('id', 'name', 'slug')
+
+
+class WatershedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Watershed
 
 
 class LocationSerializer(ModelSerializer):
 
     class Meta:
         model = Location
+
+    building_use = BuildingUseSerializer()
+    neighborhood = NeighborhoodSerializer()
+    watershed = WatershedSerializer()
