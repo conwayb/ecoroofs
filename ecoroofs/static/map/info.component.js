56336fe891313ceeb1caae0fd1cd2aa6a4f078f6
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-09-28 18:12:06 -0700

    When opening location info panel, be smarter about panning map
    This builds on work started by @conwayb. The differences from before
    are:
    
    - Only pan to the left if necessary (if the right info panel would cover
      the location that was clicked).
    - But *don't* pan if panning would push the clicked location off-screen.
    - Don't pan at all when the info panel is closed. It's not clear where
      we should pan "back" to, and not panning shouldn't cause any confusion
      for users.

--- ecoroofs/static/map/info.component.js
+++ ecoroofs/static/map/info.component.js
@@ -6,57 +6,81 @@ const ContentComponent = {
     controller: function ($mdSidenav, map, Location) {
         const mapView = map.getView();
         const sidenavId = 'sidenav-map-info';
-        const width = 320;  // XXX
+
+        // TODO: Use the current width of the sidenav instead of hard
+        // TODO: coding this. Not sure of the best way to do this since
+        // TODO: it's not easy to get the width of a sidenav.
+        const width = 320;
+        const buffer = 20;
+        const bufferedWidth = width + buffer;
 
         this.location = null;
 
-        this.pan = (direction) => {
-            const center = mapView.getCenter();
-            const centerPixel = map.getPixelFromCoordinate(center);
-            const x = centerPixel[0];
-            const y = centerPixel[1];
-            const newX = x + (width * direction);
-            const newCenter = map.getCoordinateFromPixel([newX, y]);
-            const pan = ol.animation.pan({
-                source: map.getView().getCenter(),
-                duration: 200
-            });
-            map.beforeRender(pan);
-            mapView.setCenter(newCenter);
-        };
-
-        this.open = () => {
+        // Open the right info panel. If necessary, also pan the map to
+        // the left to keep the clicked point (the "anchor point") from
+        // being covered by the info panel. At the same time, also make
+        // sure the anchor point isn't pushed off screen. On small
+        // screens, if these rules conflict, the anchor point will be
+        // covered.
+        this.open = (anchorPoint) => {
             const sidenav = $mdSidenav(sidenavId);
-            if (!sidenav.isOpen()) {
-                sidenav.open();
-                this.pan(1);
-            }
-        };
 
-        this.close = () => {
-            const sidenav = $mdSidenav(sidenavId);
             if (sidenav.isOpen()) {
-                sidenav.close();
-                this.pan(-1);
+                return;
             }
+
+            sidenav.open();
+
+            const anchorPointX = anchorPoint[0];
+            const mapWidth = map.getSize()[0];
+
+            // Only pan if:
+            //   - Opening the right sidenav *would* cover the clicked point
+            //   - Panning would *not* push the clicked point off screen (to the left)
+            const doPan = (anchorPointX > (mapWidth - bufferedWidth)) && (anchorPointX > bufferedWidth);
+
+            if (doPan) {
+                const center = mapView.getCenter();
+                const centerPixel = map.getPixelFromCoordinate(center);
+                const x = centerPixel[0];
+                const y = centerPixel[1];
+                const newX = x + bufferedWidth;
+                const newCenter = map.getCoordinateFromPixel([newX, y]);
+                const panimation = ol.animation.pan({
+                    source: map.getView().getCenter(),
+                    duration: 200
+                })
+                map.beforeRender(panimation);
+                mapView.setCenter(newCenter);
+            }
+        };
+
+        // When the info panel is closed, we intentionally don't pan
+        // because trying to figure out where we should pan back to
+        // isn't entirely clear, and it doesn't seem necessary for UX
+        // reasons (i.e., maintaining the current map view seems fine).
+        this.close = (anchorPoint) => {
+            const sidenav = $mdSidenav(sidenavId);
+            sidenav.close();
             map.clearHighlightOverlay();
         };
 
         map.on('singleclick', (event) => {
-            const feature = map.forEachFeatureAtPixel(event.pixel, (feature) => feature);
+            const pixel = event.pixel;
+            const feature = map.forEachFeatureAtPixel(pixel, (feature) => feature);
             if (feature) {
                 const slug = feature.get('slug');
                 this.location = Location.get({
                     slug: slug
                 }, () => {
-                    this.open();
+                    this.open(pixel);
                     map.clearHighlightOverlay();
                     map.highlightFeature(feature);
                 }, () => {
-                    this.close();
+                    this.close(pixel);
                 });
             } else {
-                this.close();
+                this.close(pixel);
             }
         });
     }
