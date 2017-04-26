07ab0565f29468cd4ac6d0b7a6e9b0f517078abd
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-26 13:04:54 -0700

    Upgrade Python dependencies

--- setup.py
+++ setup.py
@@ -14,14 +14,13 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'django>=1.10.2',
-        'django-arcutils>=2.13.0',
+        'django>=1.10.7,<1.11',
+        'django-arcutils>=2.16.0',
         'django-pgcli>=0.0.2',
-        'djangorestframework>=3.5.1',
-        'Markdown>=2.6.7',
-        'psycopg2>=2.6.2',
+        'djangorestframework>=3.6.2',
+        'Markdown>=2.6.8',
+        'psycopg2>=2.7.1',
         'psu.oit.arc.tasks',
-        'raven>=5.31.0',
     ],
     extras_require={
         'dev': [
